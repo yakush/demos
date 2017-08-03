e9a78f23624368ab56013a16c9d2753903b52979
yakush <its.yak@gmail.com>
2017-08-04 01:03:32 +0300

    ...

--- sketch.js
+++ sketch.js
@@ -3,7 +3,16 @@ var v;
 function setup() {
   noCanvas();
   createP('a');
-  v= createCapture(VIDEO);
+  //v= createCapture(VIDEO);
+  v= createCapture({
+    audio: false,
+    video: {
+      facingMode: "environment"
+      // {
+      //   exact: "environment"
+      // }
+    }
+  });
   v.style('border','1px solid black');
 
   v.id('vid');  
